d278d74611c7fc9790a8476603d21cc7d77cae8b
Baralabs <fbara@icloud.com>
2023-08-26 18:27:59 -0400

    Initial Commit

---
+++ MyTesla/MyTeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTeslaApp.swift
+//  MyTesla
+//
+//  Created by Frank Bara on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MyTeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
